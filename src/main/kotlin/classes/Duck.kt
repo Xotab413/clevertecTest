5748fcbcec2eee98110f5eaa2eb38c443f248284
Kasheuski Arseni <30667022+Belarussianin@users.noreply.github.com>
2022-01-21 19:48:53 +0300

    Hot fix

--- src/main/kotlin/classes/Duck.kt
+++ src/main/kotlin/classes/Duck.kt
@@ -6,7 +6,18 @@ import cleverTechTest.basicClasses.Bird
 import cleverTechTest.interfaces.Swimmable
 
 class Duck(initState: AnimalState? = null) : Bird(initState), Swimmable {
+
     override fun makeAnimal(initState: AnimalState?): Animal = Duck(initState)
+
+    override fun randomAction() {
+        when ((1..5).random()) {
+            1 -> fly()
+            2 -> eat()
+            3 -> sleep()
+            4 -> move()
+            5 -> swim()
+        }
+    }
 
     override fun swim() {
         state = AnimalState.SWIMMING
--- src/main/kotlin/classes/Wolf.kt
+++ src/main/kotlin/classes/Wolf.kt
@@ -6,7 +6,18 @@ import cleverTechTest.basicClasses.Mammal
 import cleverTechTest.interfaces.Swimmable
 
 class Wolf(initState: AnimalState? = null) : Mammal(initState), Swimmable {
+
     override fun makeAnimal(initState: AnimalState?): Animal = Wolf(initState)
+
+    override fun randomAction() {
+        when ((1..5).random()) {
+            1 -> feed()
+            2 -> eat()
+            3 -> sleep()
+            4 -> move()
+            5 -> swim()
+        }
+    }
 
     override fun swim() {
         state = AnimalState.SWIMMING
